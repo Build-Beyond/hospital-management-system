3b9d43cf06652931d76ce13d27631c85dffe8a49
Build-Beyond <akolesanchita@gmail.com>
2025-08-18 23:30:01 +0530

    Initial setup of project

---
+++ Frontend/hms/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-xl shadow-lg text-center">
+      🚀 Vite + React!
+    </div>
+  )
+}
+
+export default App
